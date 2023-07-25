3bfe9b744a8fb6e39f5e1169fb9ff8c311cfd15a
Sena Kobayashi <231059@st.yoshida-g.ac.jp>
2023-07-25 11:14:22 +0900

    first commit

---
+++ src/main/java/jp/yoshida/shop/Shop75Application.java
@@ -0,0 +1,13 @@
+package jp.yoshida.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop75Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop75Application.class, args);
+	}
+
+}
